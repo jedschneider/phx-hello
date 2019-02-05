02c3da3aee6046a66c48bead0cf16be733e828cb
Jed Schneider <jed.schneider@gmail.com>
2019-02-05 11:37:31 -0800

    make port dynamic

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 config :hello, HelloWeb.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  http: [:inet6, port: 4001],
-  url: [host: "boiling-woodland-24991.herokuapp.com", port: 4001],
+  http: [:inet6, port: System.get_env("PORT")],
+  url: [host: "boiling-woodland-24991.herokuapp.com", port: System.get_env("PORT")],
   cache_static_manifest: "priv/static/cache_manifest.json",
   root: ".",
   server: true,
